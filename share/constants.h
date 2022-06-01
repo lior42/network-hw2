88cee2677c12922b6cd21d6b82ed10460e67caef
lior42 <liorlander@gmail.com>
2022-06-02 01:55:23 +0300

    Added some shared functionality and fixed makefile

---
+++ share/constants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <stdint.h>
+
+static const uint16_t SERVER_PORT = 8888;
+
+// constant pointer to a constant...
+static const char *const SERVER_ADDRESS = "127.0.0.1";
---
+++ share/err_handler.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef ERRS_ON_STDERR
+#define HW2_SHARE_ERR_OUTPUT stdout
+#else
+#define HW2_SHARE_ERR_OUTPUT stderr
+#endif
+
+inline void validate_or_die(bool cond, const char *fail_message, ...) {
+    if (!cond) {
+        va_list ap;
+        va_start(ap, fail_message);
+        vfprintf(HW2_SHARE_ERR_OUTPUT, fail_message, ap);
+        va_end(ap);
+        fprintf(HW2_SHARE_ERR_OUTPUT, "\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+#undef HW2_SHARE_ERR_OUTPUT
