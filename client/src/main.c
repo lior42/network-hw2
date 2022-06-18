5d60708e449cfa7c412b0ec557766d6120344613
lior42 <liorlander@gmail.com>
2022-06-18 16:44:57 +0300

    Temporary, sent for evaluation

--- client/src/main.c
+++ client/src/main.c
@@ -6,90 +6,84 @@
 
 // void printf_nl(const char *idk) { printf("%s\n", idk); }
 
+#include "logger.h"
+#include "shared.h"
+#include <arpa/inet.h>
 #include <netdb.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/poll.h>
 #include <sys/socket.h>
 #include <time.h>
-#include "logger.h"
+
+#include <poll.h>
+
 #define MAX 80
 #define PORT 8888
 #define SA struct sockaddr
-#define bzero(a, b) memset(a, EOF, b)
 
+void func(FILE *sockfd) {
+    struct pollfd p = {.fd = fileno(sockfd), .events = POLLIN};
+    // char buff[MAX];
+    char *tmp_u;
+    // int n;
+    for (;;) {
+        scanf("%ms", &tmp_u);
+        // strcpy(tmp_u, "Food\n");
+        if (tmp_u != NULL) {
+            fputs(tmp_u, sockfd);
+            fputc('\n', sockfd);
+        }
+        fflush(sockfd);
+        logger(tmp_u);
+        free(tmp_u);
+        tmp_u = NULL;
 
-void delay(int number_of_seconds)
-{
-    // Converting time into milli_seconds
-    int milli_seconds = 1000 * number_of_seconds;
+        poll(&p, 1, -1);
+        printf("{.events = %d, .revents = %d}\n", p.events, p.revents);
 
-    // Storing start time
-    clock_t start_time = clock();
-
-    // looping till required time is not achieved
-    while (clock() < start_time + milli_seconds)
-        ;
-}
-void func(FILE * sockfd)
-{
-	// char buff[MAX];
-	char * tmp_u = calloc(strlen("Food\n")+1, sizeof(char));
-	// int n;
-	for (;;) {
-		// scanf("%ms",&tmp_u);
-		strcpy(tmp_u, "Food\n");
-		if(tmp_u != NULL)
-		{
-			fputs(tmp_u, sockfd);
-		}
-		fflush(sockfd);
-		logger(tmp_u);
-		free(tmp_u);
-		delay(5);
-		fscanf(sockfd, "%ms", &tmp_u);
-		// logger(tmp_u);
-		if(tmp_u != NULL)
-		{
-			printf("%s\n", tmp_u);
-		}
-		free(tmp_u);
-		break;
-	}
+        if (p.revents & POLLIN) {
+            fscanf(sockfd, "%ms", &tmp_u);
+            if (tmp_u != NULL) {
+                printf("%s\n", tmp_u);
+            }
+            logger(tmp_u);
+            free(tmp_u);
+        }
+    }
 }
 
-int main()
-{
-	int sockfd, connfd;
-	struct sockaddr_in servaddr, cli;
+int main() {
+    int sockfd;
+    struct sockaddr_in servaddr;
 
-	// socket create and verification
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd == -1) {
-		printf("socket creation failed...\n");
-		exit(0);
-	}
-	else
-		printf("Socket successfully created..\n");
-	bzero(&servaddr, sizeof(servaddr));
+    // socket create and verification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    } else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
 
-	// assign IP, PORT
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	servaddr.sin_port = htons(PORT);
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_port = htons(PORT);
 
-	// connect the client socket to server socket
-	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
-		printf("connection with the server failed...\n");
-		exit(0);
-	}
-	else
-		printf("connected to the server..\n");
+    // connect the client socket to server socket
+    if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) != 0) {
+        printf("connection with the server failed...\n");
+        exit(0);
+    } else
+        printf("connected to the server..\n");
 
-	// function for chat
-	FILE *s=fdopen(sockfd, "r+");
-	func(s);
+    // function for chat
+    FILE *s = fdopen(sockfd, "r+");
+    func(s);
 
-	// close the socket
-	fclose(s);
+    // close the socket
+    fclose(s);
 }
--- server/src/loops.c
+++ server/src/loops.c
@@ -1,10 +1,11 @@
 #include "loops.h"
 #include "grocerylist.h"
-#include "shared.h"
 #include "logger.h"
+#include "shared.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/poll.h>
 #include <sys/socket.h>
 
 static void clientLoop(FILE *client_io, GroceryList *data);
@@ -21,36 +22,42 @@ void serverLoop(int server_fd, GroceryList *data) {
 }
 
 static void clientLoop(FILE *client_io, GroceryList *data) {
+    struct pollfd p = {.fd = fileno(client_io), .events = POLLIN};
+    char *tmp;
+
     for (;;) {
-        char *tmp;
         // %ms = malloced string.. useful in this case.
-        fscanf(client_io, "%ms", &tmp);
-        logger(tmp);
-        if(tmp == NULL)
-        {
-            free(tmp);
-            break;
-        }
-        if (strcmp(
-                tmp, SERVER_END_COMMUNICATION
-            ) == 0) {
-            printf("Client ended conversation.\n");
-            free(tmp);
-            break;
-        }
+        poll(&p, 1, -1);
+        printf("{.events = %d, .revents = %d}\n", p.events, p.revents);
+        if (p.revents & POLLIN) {
+            fscanf(client_io, "%ms", &tmp);
+            logger(tmp);
+            if (tmp == NULL) {
+                free(tmp);
+                break;
+            }
+            if (strcmp(tmp, SERVER_END_COMMUNICATION) == 0) {
+                printf("Client ended conversation.\n");
+                free(tmp);
+                break;
+            }
 
-        Grocery *resp = groceryListSearch(data, tmp);
+            Grocery *resp = groceryListSearch(data, tmp);
 
-        if (resp != NULL) {
-            char *tmp2 = groceryToString(resp);
-            puts(tmp2);
-            fputs(tmp2, client_io);
-            free(tmp2);
-            groceryDestroy(resp);
-        } else {
-            fputs("No matching grocery found.", client_io);
+            if (resp != NULL) {
+                char *tmp2 = groceryToString(resp);
+                fprintf(client_io, "%s\n", tmp2);
+                fflush(client_io);
+                free(tmp2);
+                groceryDestroy(resp);
+            } else {
+                fputs("No matching grocery found.\n", client_io);
+            }
+
+            fflush(client_io);
+
+            free(tmp);
+            tmp = NULL;
         }
-        fflush(client_io);
-        free(tmp);
     }
 }
